f3824699106eb9f334731931ebaab38cd04c2466
Sergey Chilingaryan <sergey@sergeys-macbook-pro.local>
2018-07-03 14:08:26 +0400

    Initial Commit

---
+++ Serializer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Serializer
+//
+//  Created by Sergey Chilingaryan on 7/3/18.
+//  Copyright © 2018 Hovhannes Tsakanyan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
